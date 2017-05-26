af51c0f92a807f1d86a62ffd60f042f788f73063
Josep Gayà <josepg@limit.es>
2017-05-26 11:22:46 +0200

    #72 No es pot crear un document físic

--- ripea-core/src/main/java/es/caib/ripea/core/helper/ContingutHelper.java
+++ ripea-core/src/main/java/es/caib/ripea/core/helper/ContingutHelper.java
@@ -199,11 +199,13 @@ public class ContingutHelper {
 			dto.setCustodiaId(document.getCustodiaId());
 			dto.setCustodiaUrl(
 					pluginHelper.arxiuDocumentGenerarUrlPerCsv(document.getCustodiaCsv()));
-			dto.setFitxerNom(document.getFitxerNom());
-			dto.setFitxerNomEnviamentPortafirmes(
-					pluginHelper.conversioConvertirPdfArxiuNom(document.getFitxerNom()));
-			dto.setFitxerContentType(document.getFitxerContentType());
-			//dto.setFitxerContingut(document.getFitxerContingut());
+			if (document.getFitxerNom() != null) {
+				dto.setFitxerNom(document.getFitxerNom());
+				dto.setFitxerNomEnviamentPortafirmes(
+						pluginHelper.conversioConvertirPdfArxiuNom(document.getFitxerNom()));
+				dto.setFitxerContentType(document.getFitxerContentType());
+				//dto.setFitxerContingut(document.getFitxerContingut());
+			}
 			dto.setDataCaptura(document.getDataCaptura());
 			dto.setVersioDarrera(document.getVersioDarrera());
 			dto.setVersioCount(document.getVersioCount());
