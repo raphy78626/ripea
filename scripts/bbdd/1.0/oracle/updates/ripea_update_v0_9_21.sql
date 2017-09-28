5afcff7332058f2a87e9db7cdd75a7855b1e006d
Josep Gayà <josepg@limit.es>
2017-09-28 15:30:21 +0200

    94: Canviar el tipus de la propietat numero de RegistreAnotacio a String. Corregir detall
    Task-Url: https://github.com/GovernIB/ripea/issues/94
    
    Correcció de l'script

--- scripts/bbdd/1.0/oracle/updates/ripea_update_v0_9_21.sql
+++ scripts/bbdd/1.0/oracle/updates/ripea_update_v0_9_21.sql
@@ -6,12 +6,18 @@
 ALTER TABLE IPA_REGISTRE ADD NUMERO_temp VARCHAR2(100 CHAR);
 -- Copia els valors a la nova columna
 UPDATE IPA_REGISTRE SET NUMERO_temp = TO_CHAR(NUMERO);
+-- Esborra la restricció not null
+ALTER TABLE IPA_REGISTRE MODIFY (NUMERO NULL);
+-- Esborra la restricció unique de 3 columnes
+ALTER TABLE IPA_REGISTRE DROP CONSTRAINT IPA_REG_MULT_UK
 -- Esborra la columna antiga
 ALTER TABLE IPA_REGISTRE DROP COLUMN NUMERO;
 -- Reanomena la columna auxiliar com a la columna vàlida
 ALTER TABLE IPA_REGISTRE RENAME COLUMN NUMERO_temp TO NUMERO;
 -- Afegeix la restricció de not null
 ALTER TABLE IPA_REGISTRE MODIFY NUMERO NOT NULL;
+-- Restaura la restricció IPA_REG_MULT_UK
+ALTER TABLE IPA_REGISTRE ADD CONSTRAINT constraint_name UNIQUE (ENTITAT_CODI, LLIBRE_CODI, TIPUS, NUMERO, DATA);
 
 ------
 --- #95 Mostrar data i oficina d'origen al detall de l'anotació
