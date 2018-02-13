41690822850fef96d95141cf026a9e6a7b5a0748
Josep Gayà <josepg@limit.es>
2018-02-13 12:08:15 +0100

    #133: Contingut no es veu dins la bústia corresponent
    Task-Url: https://github.com/GovernIB/ripea/issues/133

--- ripea-core/src/main/java/es/caib/ripea/core/helper/ContingutHelper.java
+++ ripea-core/src/main/java/es/caib/ripea/core/helper/ContingutHelper.java
@@ -841,6 +841,13 @@ public class ContingutHelper {
 			ContingutEntity contingut,
 			ContingutEntity desti,
 			String comentari) {
+		
+		UsuariEntity usuariAutenticat = usuariHelper.getUsuariAutenticat();
+		if (usuariAutenticat == null && contingut.getDarrerMoviment() != null)
+			usuariHelper.generarUsuariAutenticat(
+					contingut.getDarrerMoviment().getRemitent().getCodi(), 
+					true);
+		
 		ContingutMovimentEntity contenidorMoviment = ContingutMovimentEntity.getBuilder(
 				contingut,
 				contingut.getPare(),
