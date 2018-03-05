f3279f355772c2751b4f9ea2e4ab6d307a72176f
Josep Gayà <josepg@limit.es>
2018-03-05 09:49:18 +0100

    #135: Error valor mime massa gran
    Task-Url: https://github.com/GovernIB/ripea/issues/135

---
+++ scripts/bbdd/1.0/oracle/updates/ripea_update_v0_9_41.sql
@@ -0,0 +1,5 @@
+-----------------------------------------------------------------
+-- #135 Error. Valor mime massa gran
+-----------------------------------------------------------------
+ALTER TABLE IPA_REGISTRE_ANNEX MODIFY (FITXER_MIME VARCHAR2(100));
+
---
+++ scripts/bbdd/1.0/postgres/updates/ripea_update_v0_9_41.sql
@@ -0,0 +1,5 @@
+-----------------------------------------------------------------
+-- #135 Error. Valor mime massa gran
+-----------------------------------------------------------------
+ALTER TABLE IPA_REGISTRE_ANNEX ALTER COLUMN FITXER_MIME TYPE character varying(100);
+
