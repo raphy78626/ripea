8518e6e519297c34676406cd68974ed01cf8ddd7
Josep Gayà <josepg@limit.es>
2018-07-02 12:15:35 +0200

    #160: Pantalla alternativa de gestió de bústies
    Task-Url: https://github.com/GovernIB/ripea/issues/160
    
    merging with ripea-1.0 (first prediction of sincro)

---
+++ scripts/bbdd/1.0/postgres/updates/ripea_update_v0_9_57_2.sql
@@ -0,0 +1,11 @@
+-----------------------------------------------------------------
+-- 	#155: Poder ordenar bústies per nom d'unitat organitzat
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+-- 	#165: Gestió i Sincronització de UO
+-----------------------------------------------------------------
+
+	
+UPDATE IPA_BUSTIA SET UNITAT_ID = (SELECT ID FROM IPA_UNITAT_ORGANITZATIVA WHERE CODI = IPA_BUSTIA.UNITAT_CODI);
+   
+UPDATE IPA_REGLA SET UNITAT_ID = (SELECT ID FROM IPA_UNITAT_ORGANITZATIVA WHERE CODI = IPA_REGLA.UNITAT_CODI);
